c985fa4e766c1bb8fbc7039b7cde81e1407475a5
Mann-999 <shady.mann07@gmail.com>
2022-09-03 23:08:30 +0530

    variables problems

---
+++ variables/problem2.js
@@ -0,0 +1,4 @@
+var name = "Mann gupta";
+var name = "sunil gupta";
+var name = "Ritu gupta";
+console.log(name);
---
+++ variables/problem3.js
@@ -0,0 +1,9 @@
+var name = "Mann";
+console.log(name);
+
+var age = 18;
+console.log(age);
+
+console.log(typeof(name));
+console.log(typeof(age));
+
